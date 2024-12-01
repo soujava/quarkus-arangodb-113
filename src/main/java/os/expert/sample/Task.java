13361f6d6c2305621ab58e8df9f561e7cb871535
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-01 20:07:52 +0000

    chore: update pom.xml
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/sample/Task.java
+++
@@ -1,8 +0,0 @@
-package os.expert.sample;
-
-import jakarta.nosql.Column;
-import jakarta.nosql.Embeddable;
-
-@Embeddable(Embeddable.EmbeddableType.GROUPING)
-public record Task(@Column String title, @Column String description) {
-}
