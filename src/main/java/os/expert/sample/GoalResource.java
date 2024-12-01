a98a6d87aeed810fc9de3144883f67e411be1acb
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-01 19:12:30 +0000

    feat: create resource to goal
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/sample/GoalResource.java
+++ src/main/java/os/expert/sample/GoalResource.java
@@ -1,4 +1,28 @@
 package os.expert.sample;
 
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Path("/goals")
 public class GoalResource {
+
+    private static final Logger LOGGER = Logger.getLogger(GoalResource.class.getName());
+
+    @GET
+    public List<Goal> goals(@QueryParam("page") int page,@QueryParam("size") int size) {
+        LOGGER.info("Listing goals, page: " + page + ", size: " + size);
+        return List.of(new Goal("Learn Quarkus", "Learn Quarkus", List.of(new Task("Create a new project", "Create a new project"))));
+    }
+
+    @PUT
+    public Goal create(Goal goal) {
+        LOGGER.info("Creating a goal: " + goal);
+        return goal;
+    }
 }
